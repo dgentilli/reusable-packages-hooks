1cbb24fc25b4106f1afd26559f814467dd771548
Dave Gentilli <dgentilli@gmail.com>
2024-07-20 18:56:32 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ReusableHooks',
+      fileName: (format) => `reusable-hooks.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+});
